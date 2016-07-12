c8a160849ff12430b07a19514e3c04d0f4478b2b
Martin Hauser <mh@wrongexit.de>
2016-07-12 09:59:52 +0200

    [cli] Add tests for Column and Board
    - Also fix minor sorting error.

---
+++ cli_client/krcli/test/column_test.exs
@@ -0,0 +1,19 @@
+defmodule ColumnTest do
+  use ExUnit.Case, async: true
+  doctest Krcli.Column
+
+  test "Should create column from hash" do
+    col = Krcli.Column.create(%{"id" => 5, "name" => "blah",
+      "position" => 5})
+    assert col.name == "blah"
+    assert col.id == 5
+    assert col.position == 5
+  end
+
+  test "Should sort columns correctly" do
+    cols = [%Krcli.Column{position: 5},
+      %Krcli.Column{position: 3}, %Krcli.Column{position: 7}]
+    {:ok, ncols} = Krcli.Column.sort({:ok, cols})
+    assert [3, 5, 7] == Enum.map(ncols, fn(x) -> x.position end)
+  end
+end
