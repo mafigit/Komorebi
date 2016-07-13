7fb0dcf37778d337833c50cf02a0c170dc959bc3
Maximilian Fischer <mafi@inagile.org>
2016-07-13 12:47:07 +0200

    Scale stories and add boundary for dragging

--- client/app/js/story_canvas.js
+++ client/app/js/story_canvas.js
@@ -4,20 +4,67 @@ import Colors from './color';
 
 class StoryGroup extends Konva.Group {
   constructor(story, stage) {
+    var scale_factor = stage.getWidth()/1920;
     let story_column = stage.find('Group').filter((group) => {
       if (group.constructor.name == "ColumnGroup" &&
           group.my_attrs.id == story.column_id) {
         return true;
       }
     })[0];
+
+    let stories_on_column = stage.find('Group').filter((group) => {
+      if (group.constructor.name == "StoryGroup" &&
+          group.my_attrs.column_id == story.column_id) {
+        return true;
+      }
+    });
     super({
        x: 50 + story_column.position().x,
-       y: 30,
+       y: 30 + (stories_on_column.length * 140),
        width: 250,
        height: 150,
        draggable: true,
-       scaleY: 0.5,
-       scaleX: 0.5
+       scaleY: scale_factor,
+       scaleX: scale_factor,
+       dragBoundFunc: function(pos) {
+         var x_max = stage.getWidth() - Math.round(250 * scale_factor);
+         var y_max = stage.getHeight() - Math.round(150 * scale_factor);
+         var x_min = 0;
+         var y_min = 5;
+         if ((pos.x < x_min && pos.y < y_min) ||
+             (pos.x < x_min && pos.y > y_max) ||
+             (pos.x > x_max && pos.y < y_min) ||
+             (pos.x > x_max && pos.y > y_max)) {
+           return {
+             x: this.getAbsolutePosition().x,
+             y: this.getAbsolutePosition().y
+           };
+         } else if ((pos.x < x_min && pos.y > y_min) ||
+             (pos.x > x_max && pos.y < y_max)) {
+
+           return {
+             x: this.getAbsolutePosition().x,
+             y: pos.y
+           };
+         } else if ((pos.x > x_min && pos.y < y_min) ||
+             (pos.x < x_max && pos.y > y_max)) {
+
+           return {
+             x: pos.x,
+             y: this.getAbsolutePosition().y
+           };
+         } else  if(pos.x > x_min && pos.x < x_max && pos.y > y_min && pos.y < y_max){
+           return {
+             x: pos.x,
+             y: pos.y
+           };
+         } else {
+           return {
+             x: this.getAbsolutePosition().x,
+             y: this.getAbsolutePosition().y
+          };
+        }
+      }
     });
     this.my_attrs = story;
   }
@@ -30,11 +77,19 @@ class StoryCanvas {
     var story_rect = new Konva.Rect({
       width: 250,
       height: 150,
-      fill: Colors.green
+      fill: Colors.green,
+      shadowColor: 'black',
+      shadowOffsetX: 8,
+      shadowOffsetY: 8,
+      shadowBlur: 10,
+      shadowEnabled: true,
+      shadowOpacity: 0.5
     });
     this.KonvaElement.add(story_rect);
 
     var title = new Konva.Text({
+      width: 230,
+      height: 45,
       fill: '#FFFFFF',
       fontSize: 16,
       fontFamily: 'Helvetica',
@@ -48,13 +103,25 @@ class StoryCanvas {
     var points = new Konva.Text({
       x: story_rect.getWidth() - 30,
       y: 5,
-      fill: Colors.light_red,
+      fill: Colors.dark_gray,
       fontSize: 20,
       fontFamily: 'Helvetica',
       fontStyle: 'bold',
       text: story.points
     });
     this.KonvaElement.add(points);
+
+    var desc = new Konva.Text({
+      x: 5,
+      y: 50,
+      width: 245,
+      height: 100,
+      fill: 'white',
+      fontSize: 16,
+      fontFamily: 'Helvetica',
+      text: story.desc
+    });
+    this.KonvaElement.add(desc);
   }
 }
 export default StoryCanvas;
