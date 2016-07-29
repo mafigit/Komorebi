7331907907aa4a1a0896dc221ae8c5c955ce094b
Konrad Merz <kmerz@hulud.net>
2016-07-29 15:40:38 +0200

    Add story selector to header
    -with empty handler function. Still need to figure that out.

--- client/app/js/task_dialog.jsx
+++ client/app/js/task_dialog.jsx
@@ -6,31 +6,9 @@ import DatePicker from 'material-ui/DatePicker';
 import TextField from 'material-ui/TextField';
 import Ajax from  'basic-ajax';
 import ReactDOM from 'react-dom';
-import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
 import StoryPointPicker from './story_point_picker';
-
-class StorySelect extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state={select_value: this.props.story_id};
-  }
-
-  handleStoryChange= (event, index, value) => {
-    this.setState({select_value: value});
-    if (this.props.onChange) {
-      this.props.onChange(event, index, value);
-    }
-  }
-  render() {
-    return <SelectField value={this.state.select_value}
-      onChange={this.handleStoryChange}>
-      {this.props.stories.map((story, key) => {
-        return <MenuItem key={key} value={story.id} primaryText={story.name} />}
-      )}
-    </SelectField>
-  }
-}
+import StorySelect from './story_select';
 
 export default class TaskDialog extends React.Component {
   constructor(props) {
