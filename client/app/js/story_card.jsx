fa402aa94f1ecba6f3b722b96f812f9451983dcb
Stefan Thiemann <stefan.thiemann@gmail.com>
2018-01-10 23:47:00 +0100

    add an add-task-button next to the story
    -so we dont have to click to the menue anytime
     to add a task

--- client/app/js/story_card.jsx
+++ client/app/js/story_card.jsx
@@ -5,6 +5,7 @@ import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 import BoardActions from './actions/BoardActions';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import LabelIcon from 'material-ui/svg-icons/action/label';
+import AddIcon from 'material-ui/svg-icons/content/add';
 import LabelOutlineIcon from 'material-ui/svg-icons/action/label-outline';
 import {grey400} from 'material-ui/styles/colors';
 
@@ -28,6 +29,10 @@ export default class StoryCard extends React.Component {
     BoardActions.openStoryShowDialog(this.props.story_id);
   }
 
+  onClickAddTaskHandler = (event) => {
+    event.stopPropagation();
+    BoardActions.showTaskDialog();
+  }
 
   // This is needed for testing the a component without the whole app context
   static childContextTypes = {
@@ -45,9 +50,15 @@ export default class StoryCard extends React.Component {
     if (this.props.active) {
       leftIcon = <LabelIcon color={this.props.color} onClick={this.onClickColorIconHandler}/>;
     }
-    var rightIcon = (<IconButton tooltip="Show story" onClick={this.onClickIconHandler}>
+    var rightIcon = (
+      <div>
+      <IconButton tooltip="Add task" onClick={this.onClickAddTaskHandler}>
+        <AddIcon color={grey400} />
+      </IconButton>
+      <IconButton tooltip="Show story" onClick={this.onClickIconHandler}>
         <MoreVertIcon color={grey400} />
-      </IconButton>);
+      </IconButton>
+      </div>);
 
     return <ListItem
       primaryText={this.props.name}
